3ff649618ca549e51f1a3f2f6cd33ab658a21520
Mats Wikström <wikstrom.mats@gmail.com>
2019-11-23 17:25:42 +0100

    sd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Något fungerar!
+         Något fungerar efter pull request!
         </p>
         <a
           className="App-link"
